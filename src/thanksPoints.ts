04d2c50a1a5679213a87adda64dcde16034f105e
fsvreddit <john@mclusky.co.uk>
2024-03-12 10:16:30 +0000

    Handle "-" flairs properly

--- src/thanksPoints.ts
+++ src/thanksPoints.ts
@@ -120,6 +120,9 @@ export async function handleThanksEvent (event: CommentSubmit | CommentUpdate, c
     let currentScore = 0;
     if (!userFlair || !userFlair.flairText || userFlair.flairText === "-") {
         newScore = 1;
+        if (userFlair && userFlair.flairText === "-") {
+            currentScore = 0;
+        }
     } else {
         currentScore = parseInt(userFlair.flairText);
         if (isNaN(currentScore)) {
