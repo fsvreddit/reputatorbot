2be9e6b0f7d88fe76414925d4eb473a5b8b572da
fsvreddit <john@mclusky.co.uk>
2024-05-05 12:44:54 +0100

    Custom post WIP

--- src/leaderboardCustomPost.tsx
+++ src/leaderboardCustomPost.tsx
@@ -1,33 +1,50 @@
-import {Context, Devvit} from "@devvit/public-api";
+import {Context, CustomPostType, Devvit, MenuItemOnPressEvent} from "@devvit/public-api";
 import {AppSetting} from "./settings.js";
 import {POINTS_STORE_KEY} from "./thanksPoints.js";
 import {getSubredditName} from "./utility.js";
 
-export const leaderboardCustomPost: Devvit.CustomPostComponent = async (context: Context) => {
-    const settings = await context.settings.getAll();
+export async function createCustomPost (event: MenuItemOnPressEvent, context: Context) {
+    await context.reddit.submitPost({
+        subredditName: "fsvsandbox",
+        title: "ReputatorBot High Scores",
+        preview: (
+            <vstack padding="medium" cornerRadius="medium">
+                <text style="heading" size="medium">
+                    Loading ReputatorBot Leaderboard...
+                </text>
+            </vstack>
+        ),
+    });
+}
 
-    const leaderboardSize = settings[AppSetting.LeaderboardSize] as number ?? 20;
+export const leaderboardCustomPost: CustomPostType = {
+    name: "leaderboardCustomPost",
+    // description: "Post that displays ReputatorBot high scorers",
+    // height: "tall",
+    render: async context => {
+        const settings = await context.settings.getAll();
 
-    const highScores = await context.redis.zRange(POINTS_STORE_KEY, 0, leaderboardSize - 1, {by: "rank", reverse: true});
+        const leaderboardSize = settings[AppSetting.LeaderboardSize] as number ?? 20;
 
-    const subredditName = await getSubredditName(context);
+        const highScores = await context.redis.zRange(POINTS_STORE_KEY, 0, leaderboardSize - 1, {by: "rank", reverse: true});
 
-    return (
-        <vstack>
-            <text size="xxlarge">Scores for /r/{subredditName}</text>
-            <spacer size="medium" />
-            <hstack>
-                <spacer size="small" />
-                <vstack>
-                    {highScores.map(score => <text>{score.member}</text>)}
-                </vstack>
-                <spacer size="small" />
-                <vstack>
-                    {highScores.map(score => <text>{score.score}</text>)}
-                </vstack>
-            </hstack>
-        </vstack>
-    );
+        const subredditName = await getSubredditName(context);
+
+        return (
+            <vstack>
+                <text size="xxlarge">Scores for /r/{subredditName}</text>
+                <spacer size="medium" />
+                <hstack>
+                    <spacer size="small" />
+                    <vstack>
+                        {highScores.map(score => <text>{score.member}</text>)}
+                    </vstack>
+                    <spacer size="small" />
+                    <vstack>
+                        {highScores.map(score => <text>{score.score}</text>)}
+                    </vstack>
+                </hstack>
+            </vstack>
+        );
+    },
 };
-
-export default Devvit;
