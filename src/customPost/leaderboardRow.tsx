cf81cc5eb4be6109a7a53b3d792401261a9b4294
fsvreddit <john@mclusky.co.uk>
2024-05-07 07:15:15 +0100

    Fix text truncation issue

--- src/customPost/leaderboardRow.tsx
+++ src/customPost/leaderboardRow.tsx
@@ -9,13 +9,10 @@ interface LeaderboardItem {
 
 export const LeaderboardRow = (props: LeaderboardItem) => (
     <hstack cornerRadius="small" gap="small" width="100%" minWidth="100%" grow>
-        <zstack alignment="middle center" height={"36px"} minWidth={"32px"} cornerRadius="full" border="thick">
-            <text alignment="middle center">{props.rank}</text>
-        </zstack>
-        <spacer grow/>
+        <spacer size="small"/>
         <button onPress={props.navigateToProfile}>{props.username}</button>
         <spacer grow/>
-        <zstack alignment="middle center" height={"36px"} minWidth={"32px"} cornerRadius="full">
+        <zstack alignment="middle center" cornerRadius="full">
             <text>{props.score} points</text>
         </zstack>
         <spacer size="small"/>
