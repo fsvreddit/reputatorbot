67424753239d7558635ca04e61cb076f46c49602
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2026-04-16 14:59:57 +0100

    fix: ensure cleanup of recently run tasks on app installation

--- src/server/triggers/appInstall.ts
+++ src/server/triggers/appInstall.ts
@@ -4,5 +4,7 @@ import type { Context } from "hono";
 export const onAppInstall = async (c: Context) => {
     await redis.set("InstallDate", new Date().getTime().toString());
 
+    await redis.del("cleanupRecentlyRun");
+
     return c.json({ message: "app installed" }, 200);
 };
